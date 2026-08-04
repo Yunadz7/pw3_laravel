d37521e773353fc921bc6bbf1b2836bef38e87f7
Yuna <yamamaruyuna@gmail.com>
2026-08-04 14:13:03 -0300

    wip: layout padrão com css

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@ use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/produtos', [ProdutoController::class , 'index']);
