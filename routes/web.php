c83a68592165359fc8f4d51886bb4a9a2e171c79
Yuna <yamamaruyuna@gmail.com>
2026-08-10 15:02:21 -0300

    wip:tailwind

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::view('/landing', 'landing');
+
 Route::get('/produtos', [ProdutoController::class , 'index']);
 Route::post('/produtos', [ProdutoController::class , 'store']);
