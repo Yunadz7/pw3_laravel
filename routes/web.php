aef883723bff2f3eb6a26d8ad359fc731f019ebd
Yuna <yamamaruyuna@gmail.com>
2026-09-08 13:57:12 -0300

    feat: CRUD Create

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 
@@ -10,7 +11,15 @@ Route::get('/', function () {
 Route::view('/landing', 'landing');
 Route::view('/admin', 'admin.dashboard');
 
+//rota para carregar o formulário get
+Route::get('/usuarios/novo',[UserController::class, 'create']);
+
+//rota para salvar os dados enviados POST
+Route::post('/usuarios', [UserController::class , 'store']);
+
+
 Route::get('/produtos', [ProdutoController::class , 'index']);
+
 Route::post('/produtos', [ProdutoController::class , 'store']);
 
 Route::get('/teste-orm', function(){
